c8b191127506cab33447969f09481197f9e56ba5
Rafa <joser210507@gmail.com>
2021-01-19 20:10:21 -0400

    Finished

--- config/db.config.js
+++ config/db.config.js
@@ -7,53 +7,12 @@ const mssql = require('mssql');
 // }
 
 const config = {
-  user: 'sa',
-  password: 'academic123',
-  server: '10.100.100.8',
-  database: 'academic_project',
+  user: 'xxxxx',
+  password: 'xxxxx',
+  server: 'xxxxx',
+  database: 'xxxxx',
 };
 
-
-// const dbConn = mssql.connect(config);
-
-// const dbConn = () => {
-//   let pool = null;
-
-//   async function getConnection() {
-//     if (pool) {
-//       return pool;
-//     }
-
-//     try {
-
-//       pool = await mssql.connect(config);
-
-//       pool.on("error", async (error) => {
-//         console.log(err);
-//         await closeConnection();
-//       });
-
-//       return pool;
-
-//     } catch (err) {
-
-//       console.log(err);
-//       pool = null;
-
-//     }
-//   }
-
-//   async function closeConnection() {
-
-//     try {
-//       await pool.close();
-//       pool = null;
-//     } catch (err) {
-//       pool = null;
-//       console.log(err);
-//     }
-//   }
-// }
 const pool = new mssql.ConnectionPool(config);
 
 module.exports = pool;
